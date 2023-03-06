489fe1de399fdaa511c048d70a260f1a97e3dd85
BenPyton <ben.pyton@gmail.com>
2023-03-06 18:57:09 +0100

    Improved plugin' settings

--- Source/ProceduralDungeon/Public/ProceduralDungeonSettings.h
+++ Source/ProceduralDungeon/Public/ProceduralDungeonSettings.h
@@ -36,36 +36,50 @@ class PROCEDURALDUNGEON_API UProceduralDungeonSettings : public UObject
 public:
 	UProceduralDungeonSettings(const FObjectInitializer& ObjectInitializer);
 
-	UPROPERTY(EditAnywhere, config, Category = "Procedural Dungeon")
+	// Size of a room unit. Room's size in data assets will express the multiple of this unit size.
+	// For example a room size of (5, 10, 1) with a unit size of (100, 100, 400) will result of a real room size of (500, 1000, 400).
+	UPROPERTY(EditAnywhere, config, Category = "General")
 	FVector RoomUnit;
 
-	UPROPERTY(EditAnywhere, config, Category = "Procedural Dungeon")
+	// The bounding size of the doors. It is used only to display the door's blue box when "Draw Debug" is set to true.
+	UPROPERTY(EditAnywhere, config, Category = "General")
 	FVector DoorSize;
 
-	UPROPERTY(EditAnywhere, config, Category = "Procedural Dungeon")
+	// The height of the door's base from the room's base (in percentage of the room unit Z)
+	UPROPERTY(EditAnywhere, config, Category = "General")
 	float DoorOffset;
 
-	UPROPERTY(EditAnywhere, config, Category = "Procedural Dungeon", meta=(Tooltip = "The rooms visibility will be toggled off when the player is not inside it or in a room next to it."))
+	// The rooms visibility will be toggled off when the player is not inside it or in a room next to it.
+	UPROPERTY(EditAnywhere, config, Category = "Occlusion Culling", meta = (DisplayName = "Enable Occlusion Culling"))
 	bool OcclusionCulling;
 
-	//UPROPERTY(EditAnywhere, config, Category = "Procedural Dungeon", meta=(EditCondition="OcclusionCulling", Tooltip = "The legacy occlusion culling system only toggles the visibility of the actors in the rooms, keeping the collisions, ticking and all oher things.\nThe new system toggles instead the whole room levels visibility, shutting off the ticking and other things of the actors and the level script."))
+	// The legacy occlusion culling system only toggles the visibility of the actors in the rooms, keeping the collisions, ticking and all oher things.
+	// The new system toggles instead the whole room levels visibility, shutting off the ticking and other things of the actors and the level script.
+	//UPROPERTY(EditAnywhere, config, Category = "Procedural Dungeon", meta=(EditCondition="OcclusionCulling"))
 	//bool LegacyOcclusion;
 
-	UPROPERTY(EditAnywhere, config, Category = "Procedural Dungeon")
+	// Defines the number of connected rooms are visible from the player's room (1 means only the room where the player is).
+	UPROPERTY(EditAnywhere, config, Category = "Occlusion Culling", meta=(EditCondition="OcclusionCulling"))
 	uint32 OcclusionDistance;
 
-	UPROPERTY(EditAnywhere, config, Category = "Procedural Dungeon", meta=(Tooltip = "Keep track of dynamic actors entering and leaving the room to be able to show/hide them with the room."))
+	// Keep track of dynamic actors entering and leaving the room to be able to show/hide them with the room.
+	UPROPERTY(EditAnywhere, config, Category = "Occlusion Culling", meta=(EditCondition="OcclusionCulling"))
 	bool OccludeDynamicActors;
 
-	UPROPERTY(EditAnywhere, config, Category = "Procedural Dungeon")
+	// When true, doors will be connected as long they are at the same place.
+	// When false, only the doors between the previous and the new generated room will be connected.
+	UPROPERTY(EditAnywhere, config, Category = "General")
 	bool CanLoop;
 
-	UPROPERTY(EditAnywhere, config, Category = "Procedural Dungeon")
+	// Show room and door outlines in editor and development builds
+	UPROPERTY(EditAnywhere, config, Category = "Debug")
 	bool DrawDebug;
 
-	UPROPERTY(EditAnywhere, config, Category = "Procedural Dungeon")
+	// Show some logs on the screen
+	UPROPERTY(EditAnywhere, config, Category = "Debug")
 	bool OnScreenPrintDebug;
 
-	UPROPERTY(EditAnywhere, config, Category = "Procedural Dungeon")
+	// Duration of the screen logs
+	UPROPERTY(EditAnywhere, config, Category = "Debug", meta=(EditCondition="OnScreenPrintDebug"))
 	float PrintDebugDuration;
 };
