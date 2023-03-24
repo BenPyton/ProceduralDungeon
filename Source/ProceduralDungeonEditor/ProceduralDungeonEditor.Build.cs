f49b41b9ce7d1dd495a5979cfdac7d5de8d563ea
BenPyton <ben.pyton@gmail.com>
2023-03-24 16:26:21 +0100

    Added an Editor Module and customized how the DoorDef details draws

---
+++ Source/ProceduralDungeonEditor/ProceduralDungeonEditor.Build.cs
@@ -0,0 +1,21 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class ProceduralDungeonEditor : ModuleRules
+{
+	public ProceduralDungeonEditor(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "Projects" });
+			
+		PrivateDependencyModuleNames.AddRange(new string[] {
+				"ProceduralDungeon",
+				"CoreUObject",
+				"PropertyEditor",
+				"DetailCustomizations"
+			}
+		);
+	}
+}
