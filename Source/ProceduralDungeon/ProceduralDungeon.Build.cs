40f2fbc8ce5d146609cd861c61629ff5ae7bcdf2
BenPyton <BenPyton@users.noreply.github.com>
2025-03-26 14:13:42 +0100

    Changed license from "MIT" to "CeCILL-C or Fab"

--- Source/ProceduralDungeon/ProceduralDungeon.Build.cs
+++ Source/ProceduralDungeon/ProceduralDungeon.Build.cs
@@ -1,4 +1,12 @@
-// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*
+ * Copyright (c) 2019-2024 Benoit Pelletier
+ *
+ * This software is available under different licenses depending on the source from which it was obtained:
+ * - The Fab EULA (https://fab.com/eula) applies when obtained from the Fab marketplace.
+ * - The CeCILL-C license (https://cecill.info/licences/Licence_CeCILL-C_V1-en.html) applies when obtained from any other source.
+ *
+ * Please refer to the accompanying LICENSE file for further details.
+ */
 
 using UnrealBuildTool;
 
