a5ef98cc81c7a901effa86500e3ffc66e8d77d8f
BenPyton <BenPyton@users.noreply.github.com>
2025-06-04 17:05:31 +0200

    Updated copyright headers to comply with Fab TRC

--- Source/ProceduralDungeon/Private/DungeonGeneratorBase.cpp
+++ Source/ProceduralDungeon/Private/DungeonGeneratorBase.cpp
@@ -1,12 +1,9 @@
-/*
- * Copyright (c) 2019-2025 Benoit Pelletier
- *
- * This software is available under different licenses depending on the source from which it was obtained:
- * - The Fab EULA (https://fab.com/eula) applies when obtained from the Fab marketplace.
- * - The CeCILL-C license (https://cecill.info/licences/Licence_CeCILL-C_V1-en.html) applies when obtained from any other source.
- *
- * Please refer to the accompanying LICENSE file for further details.
- */
+// Copyright Benoit Pelletier 2019 - 2025 All Rights Reserved.
+//
+// This software is available under different licenses depending on the source from which it was obtained:
+// - The Fab EULA (https://fab.com/eula) applies when obtained from the Fab marketplace.
+// - The CeCILL-C license (https://cecill.info/licences/Licence_CeCILL-C_V1-en.html) applies when obtained from any other source.
+// Please refer to the accompanying LICENSE file for further details.
 
 #include "DungeonGeneratorBase.h"
 #include "Engine/Engine.h" // GEngine
